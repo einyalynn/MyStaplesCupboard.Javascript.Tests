aa9a7d5514ae45a19d2c512e2188afb0351ad727
Dyana Rose <dyanarose@gmail.com>
2012-12-07 00:04:01 +0000

    initial commit

---
+++ scripts/measurement.js
@@ -0,0 +1,8 @@
+﻿var measurementLib = {
+    costPerUnit: function (cost, units, decimalPlaces) {
+        var precision = decimalPlaces || 2;
+        if(!isNaN(cost) && !isNaN(units)){
+            return (parseFloat(cost) / parseFloat(units)).toPrecision(precision);
+        }
+    }
+};
---
+++ tests/measurementTests.js
@@ -0,0 +1,13 @@
+﻿///<reference path="../scripts/measurement.js" />
+test("will get cost per unit", function () {
+    var res = measurementLib.costPerUnit(1.00, 2, 3);
+
+    equal(res, 0.50, "should give cost per unit to 3 decimal precision");
+    equal(res.split('.')[1].length, 3, "should give cost per unit to 3 decimal precision: actual " + res.split('.')[1].length);
+});
+test("will get cost per unit without specifing precision", function () {
+    var res = measurementLib.costPerUnit(1.00, 2);
+
+    equal(res, 0.50, "should give cost per unit to 2 decimal precision");
+    equal(res.split('.')[1].length, 2, "should give cost per unit to 2 decimal precision: actual " + res.split('.')[1].length);
+});
