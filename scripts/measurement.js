a500c7e63a77c124152a8bbb16e8032be816f4f8
Dyana Rose <dyanarose@gmail.com>
2012-12-07 17:27:53 +0000

    testing conversion to currency

--- scripts/measurement.js
+++ scripts/measurement.js
@@ -2,7 +2,18 @@
     costPerUnit: function (cost, units, decimalPlaces) {
         var precision = decimalPlaces || 2;
         if(!isNaN(cost) && !isNaN(units)){
-            return (parseFloat(cost) / parseFloat(units)).toPrecision(precision);
+            return (parseFloat(cost) / parseFloat(units)).toFixed(precision);
+        }
+    },
+    normalizeCost: function (cost, formatting) {
+        if (!isNaN(cost)) {
+            var costf = parseFloat(cost);
+            var costInt = parseInt(cost.toString().split('.')[0]);
+            var lessThanOne = !costInt;
+            if (lessThanOne) {
+                return formatting.minorCurrency.format((costf * 100).toFixed(1));
+            } 
+            return formatting.majorCurrency.format(costf.toFixed(2));
         }
     }
 };
--- tests/measurementTests.js
+++ tests/measurementTests.js
@@ -1,4 +1,10 @@
-﻿///<reference path="../scripts/measurement.js" />
+﻿///<reference path="../scripts/utilities.js" />
+///<reference path="../scripts/measurement.js" />
+
+var formatting = {
+    majorCurrency: "£{0}",
+    minorCurrency: "{0}p"
+};
 test("will get cost per unit", function () {
     var res = measurementLib.costPerUnit(1.00, 2, 3);
 
@@ -10,4 +16,19 @@ test("will get cost per unit without specifing precision", function () {
 
     equal(res, 0.50, "should give cost per unit to 2 decimal precision");
     equal(res.split('.')[1].length, 2, "should give cost per unit to 2 decimal precision: actual " + res.split('.')[1].length);
+});
+test("will translate pounds to pence when amount less than 1 pound", function () {
+    var cost = .567;
+    var res = measurementLib.normalizeCost(cost, formatting);
+    equal(res, "56.7p", "should give cost in pence: actual " + res);
+});
+test("will translate pounds when amount greater than 1 pound", function () {
+    var cost = 1.567;
+    var res = measurementLib.normalizeCost(cost, formatting);
+    equal(res, "£1.57", "should give cost in pounds: actual " + res);
+});
+test("will translate pounds when amount equal to 1 pound", function () {
+    var cost = 1.00;
+    var res = measurementLib.normalizeCost(cost, formatting);
+    equal(res, "£1.00", "should give cost in pounds: actual " + res);
 });
